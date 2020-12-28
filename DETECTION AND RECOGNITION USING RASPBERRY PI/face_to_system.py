91e1405925cf9dad7bc920dcbab74fb72a004110
Smriti <52624997+smriti1313@users.noreply.github.com>
2020-12-28 17:33:36 +0530

    Update face_to_system.py

--- DETECTION AND RECOGNITION USING RASPBERRY PI/face_to_system.py
+++ DETECTION AND RECOGNITION USING RASPBERRY PI/face_to_system.py
@@ -1,14 +1,14 @@
 # Import OpenCV2 for image processing
 import cv2
 
-# Start capturing video
+# Start capturing video 
 vid_cam = cv2.VideoCapture(0)
 
 # Detect object in video stream using Haarcascade Frontal Face
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # For each person, one face id
-face_id = 2
+face_id = 5
 
 # Initialize sample face image
 count = 0
@@ -23,14 +23,14 @@ while(True):
     gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
 
     # Detect frames of different sizes, list of faces rectangles
-    faces = face_detector.detectMultiScale(gray, 1.2, 5)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
     # Loops for each faces
     for (x,y,w,h) in faces:
 
         # Crop the image frame into rectangle
         cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
-
+        
         # Increment sample face image
         count += 1
 
@@ -45,7 +45,7 @@ while(True):
         break
 
     # If image taken reach 100, stop taking video
-    elif count>10:
+    elif count>100:
         break
 
 # Stop video
