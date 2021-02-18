71505b02bdc56b1218b684b8c1c7be53a2ce4169
Sudip Ghosh <60208804+sudipg4112001@users.noreply.github.com>
2021-02-19 01:03:19 +0530

    Google colab module removed, and detailing added
    Google colab module is removed and simpler OpenCV framework is used. Captions will be displayed along with the image.

--- Cartoonify Image/Cartoonify_face_image/Cartoonify-face_image.py
+++ Cartoonify Image/Cartoonify_face_image/Cartoonify-face_image.py
@@ -1,26 +1,25 @@
 #Importing required libraries
 import cv2
 import numpy as np
-from google.colab.patches import cv2_imshow
 
 #Reading image 
-img = cv2.imread("face.jpg")
-from skimage import io 
-io.imshow(img)
+img = cv2.imread("face.jpg") 
+cv2.imshow("Original image",img)
 
 #Converting to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-io.imshow(img)
+cv2.imshow("Converted to RGB",img)
 
 #Detecting edges of the input image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
+cv2.imshow("GrayScale",gray)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
-io.imshow(edges)
+cv2.imshow("Dectected edges",edges)
 
 #Cartoonifying the image
 color = cv2.bilateralFilter(img, 9, 250, 250)
 cartoon = cv2.bitwise_and(color, color, mask=edges)
 
-io.imshow(cartoon)
-
+cv2.imshow("After Cartoonification",cartoon)
+cv2.imwrite("Cartoon.jpg",cartoon)
