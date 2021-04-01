14a5c283d90603763bac09c2969c4b07d40f328e
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-04-01 22:32:49 +0530

    Update colorful_sketch_filter.py

--- Awesome-face-operations/Colorful Sketch Filter/colorful_sketch_filter.py
+++ Awesome-face-operations/Colorful Sketch Filter/colorful_sketch_filter.py
@@ -4,8 +4,8 @@ import cv2
 import os.path  #imported the required libraries
 
 # take path of the image as input
-img_path = input("Enter the path here:")  #example -> C:\Users\xyz\OneDrive\Desktop\project\image.jpg  
-img = cv2.imread(img_path)
+image_path = input("Enter the path here:")  #example -> C:\Users\xyz\OneDrive\Desktop\project\image.jpg  
+img = cv2.imread(image_path)
 
 #plt.imshow(img)
 #plt.show()
@@ -13,32 +13,31 @@ image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #plt.figure(figsize= (10,10))
 #plt.imshow(image)
 #plt.show()
-img_small = cv2.pyrDown(image)
+image_small = cv2.pyrDown(image)
 num_iter = 5
 for _ in range(num_iter):
-    img_small= cv2.bilateralFilter(img_small, d=9, sigmaColor=9, sigmaSpace=7)
-img_rgb = cv2.pyrUp(img_small)
-#plt.imshow(img_rgb)
+    image_small= cv2.bilateralFilter(image_small, d=9, sigmaColor=9, sigmaSpace=7)
+image_rgb = cv2.pyrUp(image_small)
+#plt.imshow(image_rgb)
 #plt.show()
 
 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-img_blur = cv2.medianBlur(img_gray, 7)
-img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 0.2)
+image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2GRAY)
+image_blur = cv2.medianBlur(image_gray, 7)
+image_edge = cv2.adaptiveThreshold(image_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 0.2)
 
-#plt.imshow(img_edge)
+#plt.imshow(image_edge)
 #plt.show()
-img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-#plt.imshow(img_edge)
+image_edge = cv2.cvtColor(image_edge, cv2.COLOR_GRAY2RGB)
+#plt.imshow(image_edge)
 #plt.show()
-# img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-     
-array = cv2.bitwise_or(img_edge, image)    #used bitwise or method
-#array = cv2.bitwise_not(image, img_edge) 
+
+result = cv2.bitwise_or(image_edge, image)    #used bitwise or method between the image_edge and image
+
 plt.figure(figsize= (10,10))
-plt.imshow(array)
+plt.imshow(result)
 plt.axis('off')
-filename = os.path.basename(img_path)
-plt.savefig("(Filtered)"+filename)  #saved file name as (Filtered)image_name.jpg
+filename = os.path.basename(image_path)
+plt.savefig("(Colorful Sketch Filtered)"+filename)  #saved file name as (Filtered)image_name.jpg
 
-plt.show()  #final filtered photo
+plt.show()  #final colorful sketch filtered photo
