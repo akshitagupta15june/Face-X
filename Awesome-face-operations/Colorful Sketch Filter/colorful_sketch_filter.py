fe7aec2d0a1d8a4b0d51a5a423fc6bf67afc6428
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-04-01 22:25:04 +0530

    Update and rename colorful_sketch_filter.ipynb to colorful_sketch_filter.py

--- Awesome-face-operations/Colorful Sketch Filter/colorful_sketch_filter.py
+++ Awesome-face-operations/Colorful Sketch Filter/colorful_sketch_filter.py
@@ -11,26 +11,26 @@ img = cv2.imread(img_path)
 #plt.show()
 image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #plt.figure(figsize= (10,10))
-plt.imshow(image)
-plt.show()
+#plt.imshow(image)
+#plt.show()
 img_small = cv2.pyrDown(image)
 num_iter = 5
 for _ in range(num_iter):
     img_small= cv2.bilateralFilter(img_small, d=9, sigmaColor=9, sigmaSpace=7)
 img_rgb = cv2.pyrUp(img_small)
-plt.imshow(img_rgb)
-plt.show()
+#plt.imshow(img_rgb)
+#plt.show()
 
 
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 img_blur = cv2.medianBlur(img_gray, 7)
 img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 0.2)
 
-plt.imshow(img_edge)
-plt.show()
+#plt.imshow(img_edge)
+#plt.show()
 img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-plt.imshow(img_edge)
-plt.show()
+#plt.imshow(img_edge)
+#plt.show()
 # img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
      
 array = cv2.bitwise_or(img_edge, image)    #used bitwise or method
