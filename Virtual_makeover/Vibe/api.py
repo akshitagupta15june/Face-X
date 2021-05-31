92244e5c10dd1782029061250787fbfe0627a684
manisha-27 <67019423+manisha-27@users.noreply.github.com>
2021-05-31 13:39:17 +0530

    Update api.py

--- Virtual_makeover/Vibe/api.py
+++ Virtual_makeover/Vibe/api.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import PIL.Image
 import dlib
 import numpy as np
@@ -29,128 +27,6 @@ face_recognition_model = face_recognition_models.face_recognition_model_location
 face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
 
 
-def _rect_to_css(rect):
-    """
-    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
-
-    :param rect: a dlib 'rect' object
-    :return: a plain tuple representation of the rect in (top, right, bottom, left) order
-    """
-    return rect.top(), rect.right(), rect.bottom(), rect.left()
-
-
-def _css_to_rect(css):
-    """
-    Convert a tuple in (top, right, bottom, left) order to a dlib `rect` object
-
-    :param css:  plain tuple representation of the rect in (top, right, bottom, left) order
-    :return: a dlib `rect` object
-    """
-    return dlib.rectangle(css[3], css[0], css[1], css[2])
-
-
-def _trim_css_to_bounds(css, image_shape):
-    """
-    Make sure a tuple in (top, right, bottom, left) order is within the bounds of the image.
-
-    :param css:  plain tuple representation of the rect in (top, right, bottom, left) order
-    :param image_shape: numpy shape of the image array
-    :return: a trimmed plain tuple representation of the rect in (top, right, bottom, left) order
-    """
-    return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
-
-
-def face_distance(face_encodings, face_to_compare):
-    """
-    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
-    for each comparison face. The distance tells you how similar the faces are.
-
-    :param face_encodings: List of face encodings to compare
-    :param face_to_compare: A face encoding to compare against
-    :return: A numpy ndarray with the distance for each face in the same order as the 'faces' array
-    """
-    if len(face_encodings) == 0:
-        return np.empty((0))
-
-    return np.linalg.norm(face_encodings - face_to_compare, axis=1)
-
-
-def load_image_file(file, mode='RGB'):
-    """
-    Loads an image file (.jpg, .png, etc) into a numpy array
-
-    :param file: image file name or file object to load
-    :param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-    :return: image contents as numpy array
-    """
-    im = PIL.Image.open(file)
-    if mode:
-        im = im.convert(mode)
-    return np.array(im)
-
-
-def _raw_face_locations(img, number_of_times_to_upsample=1, model="hog"):
-    """
-    Returns an array of bounding boxes of human faces in a image
-
-    :param img: An image (as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :param model: Which face detection model to use. "hog" is less accurate but faster on CPUs. "cnn" is a more accurate
-                  deep-learning model which is GPU/CUDA accelerated (if available). The default is "hog".
-    :return: A list of dlib 'rect' objects of found face locations
-    """
-    if model == "cnn":
-        return cnn_face_detector(img, number_of_times_to_upsample)
-    else:
-        return face_detector(img, number_of_times_to_upsample)
-
-
-def face_locations(img, number_of_times_to_upsample=1, model="hog"):
-    """
-    Returns an array of bounding boxes of human faces in a image
-
-    :param img: An image (as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :param model: Which face detection model to use. "hog" is less accurate but faster on CPUs. "cnn" is a more accurate
-                  deep-learning model which is GPU/CUDA accelerated (if available). The default is "hog".
-    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
-    """
-    if model == "cnn":
-        return [_trim_css_to_bounds(_rect_to_css(face.rect), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, "cnn")]
-    else:
-        return [_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, model)]
-
-
-def _raw_face_locations_batched(images, number_of_times_to_upsample=1, batch_size=128):
-    """
-    Returns an 2d array of dlib rects of human faces in a image using the cnn face detector
-
-    :param images: A list of images (each as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :return: A list of dlib 'rect' objects of found face locations
-    """
-    return cnn_face_detector(images, number_of_times_to_upsample, batch_size=batch_size)
-
-
-def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
-    """
-    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
-    If you are using a GPU, this can give you much faster results since the GPU
-    can process batches of images at once. If you aren't using a GPU, you don't need this function.
-
-    :param images: A list of images (each as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :param batch_size: How many images to include in each GPU processing batch.
-    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
-    """
-    def convert_cnn_detections_to_css(detections):
-        return [_trim_css_to_bounds(_rect_to_css(face.rect), images[0].shape) for face in detections]
-
-    raw_detections_batched = _raw_face_locations_batched(images, number_of_times_to_upsample, batch_size)
-
-    return list(map(convert_cnn_detections_to_css, raw_detections_batched))
-
-
 def _raw_face_landmarks(face_image, face_locations=None, model="large"):
     if face_locations is None:
         face_locations = _raw_face_locations(face_image)
@@ -168,7 +44,6 @@ def _raw_face_landmarks(face_image, face_locations=None, model="large"):
 def face_landmarks(face_image, face_locations=None, model="large"):
     """
     Given an image, returns a dict of face feature locations (eyes, nose, etc) for each face in the image
-
     :param face_image: image to search
     :param face_locations: Optionally provide a list of face locations to check.
     :param model: Optional - which model to use. "large" (default) or "small" which only returns 5 points but is faster.
@@ -198,29 +73,3 @@ def face_landmarks(face_image, face_locations=None, model="large"):
         } for points in landmarks_as_tuples]
     else:
         raise ValueError("Invalid landmarks model type. Supported models are ['small', 'large'].")
-
-
-def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
-    """
-    Given an image, return the 128-dimension face encoding for each face in the image.
-
-    :param face_image: The image that contains one or more faces
-    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
-    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
-    :param model: Optional - which model to use. "large" or "small" (default) which only returns 5 points but is faster.
-    :return: A list of 128-dimensional face encodings (one for each face in the image)
-    """
-    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
-    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
-
-
-def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
-    """
-    Compare a list of face encodings against a candidate encoding to see if they match.
-
-    :param known_face_encodings: A list of known face encodings
-    :param face_encoding_to_check: A single face encoding to compare against the list
-    :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
-    :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
-    """
-    return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
