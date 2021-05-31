5ae22c23bd5b2955deaf8353888c3b2b82339702
Neeraj Ap <65017645+NEERAJAP2001@users.noreply.github.com>
2021-05-31 19:08:40 +0530

    Commit

---
+++ Recognition-Algorithms/Face Recognition using Triplet Loss Function/webcam_feed.py
@@ -0,0 +1,19 @@
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
