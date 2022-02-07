3a7baa3974aae9aedea938056aece675e5919a67
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-02-07 19:14:32 +0530

    updated main

--- Face-Age Calculator/READEM.md
+++ Face-Age Calculator/READEM.md
@@ -2,11 +2,26 @@
 
 **Task** : To find the age of the person in the real time feed.
 
+## How does it work ? 
+Our goal here is to predict the age of a person just by looking at the image.<br> 
+You might think that it is a regression task, right?  
+But, interestingly enough we and also researchers take this as a classification problem.
+#### Let's try to understand : Why ? 
+Using an image to predict age has many uncertainties or variables so to speak, like : 
+- Camera quality 
+- Lighting, Brightness and other filters
+Or maybe the person looks quite old despite being young and vice versa. So in conclusion accuracy of age calculation would be very less in real world case if treated it as a regression problem.
+<br>
+So instead we create age ranges like 0-20 , 25-30 etc and try to classify our image into one of these categories and we find that it is much more accurate and useful in real world scenario.
 ## Workflow
 
-### Packages Required
-1. opencv-python
-2. numpy
+
+## What does each script do?
+In the Models directory we have some various types of file, their uses are described below: 
+- **pb or pbtxt files** : pb stands for protobuf. In TensorFlow, the protbuf file contains the graph definition as well as the weights of the model. Thus, a pb file is all you need to be able to run a given trained model(pbtxt means that it is in text format instead of binary).
+- **caffemodel** : A CAFFEMODEL file is a machine learning model created by Caffe. It contains an image classification or image segmentation model that has been trained using Caffe.
+- **proto file** : basically  PROTOTXT files are used to create .CAFFEMODEL files.<br><br>
+_<u>Note: Caffe is a deep learning framework, originally developed at University of California, Berkeley. It is open source, under a BSD license. It is written in C++, with a Python interface.</u>_
+
 ## Result/Working : 
 
-### Other Resources : 
--- Face-Age Calculator/main.py
+++ Face-Age Calculator/main.py
@@ -8,24 +8,34 @@ def highlightFace(net, frame, conf_threshold=0.7):
     This function detects faces on the image using the 'net' passed (if any) and returns the detection output
     as well as the cordinates of the faces detected
     '''
-    frameOpencvDnn=frame.copy()
-    frameHeight=frameOpencvDnn.shape[0]
-    frameWidth=frameOpencvDnn.shape[1]
-    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
-    net.setInput(blob)
-    detections=net.forward()
+
+    frameOpencvDnn=frame.copy()
+    #--------saving the image dimensions as height and width-------#
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+
+    #-----------blob-> Preprocessing the image to required input of the model---------#
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    net.setInput(blob) #setting the image blob as input
+
+    detections = net.forward()
+    print(detections)
+    print(detections.shape)
+
     faceBoxes=[]
-    for i in range(detections.shape[2]):
-        confidence=detections[0,0,i,2]
-        if confidence>conf_threshold:
-            x1=int(detections[0,0,i,3]*frameWidth)
-            y1=int(detections[0,0,i,4]*frameHeight)
-            x2=int(detections[0,0,i,5]*frameWidth)
-            y2=int(detections[0,0,i,6]*frameHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    # for i in range(detections.shape[2]):
+    #     confidence=detections[0,0,i,2]
+    #     if confidence>conf_threshold:
+    #         x1=int(detections[0,0,i,3]*frameWidth)
+    #         y1=int(detections[0,0,i,4]*frameHeight)
+    #         x2=int(detections[0,0,i,5]*frameWidth)
+    #         y2=int(detections[0,0,i,6]*frameHeight)
+    #         faceBoxes.append([x1,y1,x2,y2])
+    #         cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn,faceBoxes
+
+
 
 #-------Creating and Parsing through the argument passed on the terminal-------------#
 parser=argparse.ArgumentParser()
@@ -34,12 +44,12 @@ parser.add_argument('--image')
 args=parser.parse_args()
 
 #-----------Model File Paths----------------#
-faceProto="opencv_face_detector.pbtxt"
-faceModel="opencv_face_detector_uint8.pb"
-ageProto="age_deploy.prototxt"
-ageModel="age_net.caffemodel"
-genderProto="gender_deploy.prototxt"
-genderModel="gender_net.caffemodel"
+faceProto="Models/opencv_face_detector.pbtxt"
+faceModel="Models/opencv_face_detector_uint8.pb"
+ageProto="Models/age_deploy.prototxt"
+ageModel="Models/age_net.caffemodel"
+genderProto="Models/gender_deploy.prototxt"
+genderModel="Models/gender_net.caffemodel"
 
 
 #-----------Model Variables---------------#
@@ -48,12 +58,12 @@ ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)',
 genderList=['Male','Female']
 
 #-------------Creating the DNN------------#
-faceNet=cv2.dnn.readNet(faceModel,faceProto)
-ageNet=cv2.dnn.readNet(ageModel,ageProto)
-genderNet=cv2.dnn.readNet(genderModel,genderProto)
+faceNet= cv2.dnn.readNet(faceModel,faceProto)
+ageNet= cv2.dnn.readNet(ageModel,ageProto)
+genderNet= cv2.dnn.readNet(genderModel,genderProto)
 
 #---------Instantiate the Video Capture Object-----------#
-video=cv2.VideoCapture(args.image if args.image else 0)
+video=cv2.VideoCapture(args.image if args.image else 0) #check whether image was passed or not otherwise use the webcam
 padding=20
 
 while cv2.waitKey(1)<0:
@@ -62,6 +72,7 @@ while cv2.waitKey(1)<0:
         cv2.waitKey()
         break
 
+    #----------------Face Detection-----------------#
     resultImg,faceBoxes=highlightFace(faceNet,frame)
     if not faceBoxes:
         print("No face detected")
