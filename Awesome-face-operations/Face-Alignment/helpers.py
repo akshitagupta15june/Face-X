252ec49a94d20e3c765f59877578e579c620207c
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-03-02 12:25:38 +0530

    Added resize function in helpers.py

--- Awesome-face-operations/Face-Alignment/helpers.py
+++ Awesome-face-operations/Face-Alignment/helpers.py
@@ -1,5 +1,7 @@
 from collections import OrderedDict
 import numpy as np
+import cv2
+
 
 def rect_to__bb(rect):
     """
@@ -25,3 +27,34 @@ def shape_to_np(shape, dtype="int"):
     
     # return the list of (x, y)-coordinates
     return coords
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+    
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
