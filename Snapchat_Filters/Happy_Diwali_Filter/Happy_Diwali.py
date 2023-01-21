c0585c5e52d597f97fa9b2a2e225c1ea91be5d4d
Nakul Singh <105866331+nerdy-nakul@users.noreply.github.com>
2023-01-22 01:23:29 +0530

    Add files via upload

---
+++ Snapchat_Filters/Happy_Diwali_Filter/Happy_Diwali.py
@@ -0,0 +1,58 @@
+import cv2
+
+
+def main():
+
+    # Getting video feed through webcam
+    cap = cv2.VideoCapture(0)
+    while True:
+        getFrame, frame = cap.read()
+        if getFrame:
+            # Getting the dimensions of the feed through the webcam
+            width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+            img = cv2.imread('hampy_diwali.jpg', -1)
+
+            # Selecting all the 3 color channels in the frame
+            frame_mask = img[:, :, 2]
+            frame_mask_inv = cv2.bitwise_not(frame_mask)
+            mask_img = img[:, :, 0:3]
+
+            # Redefining the dimensions
+            wd = int(width)
+            ht = int(height)
+
+            # Using the redefined dimensions
+            diwali = cv2.resize(mask_img, (wd, ht))
+            mask = cv2.resize(frame_mask, (wd, ht))
+            mask_inv = cv2.resize(frame_mask_inv, (wd, ht))
+
+            # Obtaining the region-of-interest
+            y1 = 0
+            y2 = int(y1 + ht)
+            x1 = 0
+            x2 = int(x1 + wd)
+            frame0 = frame[y1:y2, x1:x2]
+            frame1 = cv2.bitwise_and(frame0, frame0, mask=mask_inv)
+            frame2 = cv2.bitwise_and(diwali, diwali, mask=mask)
+
+            # Adding filter to the frame
+            frame[y1:y2, x1:x2] = cv2.add(frame1, frame2)
+
+            cv2.imshow('Snapchat Filter Diwali', frame)
+
+            cv2.resizeWindow('Snapchat Filter Diwali', 100, 200)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
