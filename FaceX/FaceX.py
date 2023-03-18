6701cbd3033f7001673bba75e55b17e3b71274cc
NoClimax@888 <sivaramana.h2021@kgkite.ac.in>
2023-03-18 12:12:08 +0530

    cartoonify by FaceX

--- FaceX/FaceX.py
+++ FaceX/FaceX.py
@@ -1,15 +1,39 @@
-from facex import FaceX
 import cv2
+import numpy as np
+import facex as x
+# Read input image
+img = cv2.imread('3.jpg')
 
-img = cv2.imread("3.jpg")
-facex = FaceX()
+# Resize image
+img = cv2.resize(img, (500, 600))
 
-faces = facex.face_detection(img)
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-for face in faces:
-    cartoon_face = facex.cartoonify(face)
-    img[face[1]:face[3], face[0]:face[2]] = cartoon_face
+# Apply median blur to remove noise
+gray = cv2.medianBlur(gray, 5)
 
-cv2.imshow("Cartoonified Image", img)
+# Detect edges using the Canny algorithm
+edges = cv2.Canny(gray, 100, 200)
+
+# Threshold the image to create a black and white cartoon version
+ret, thresh = cv2.threshold(edges, 150, 255, cv2.THRESH_BINARY)
+
+# Create a color version of the cartoon image
+color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
+# Apply bilateral filter to smooth out colors
+color = cv2.bilateralFilter(color, 9, 250, 250)
+
+# Create final cartoonified image by combining edges and colors
+cartoon = cv2.bitwise_and(color, img)
+
+# Display the cartoonified image
+cv2.imshow('Cartoon', cartoon)
+
+# Save the cartoonified image
+cv2.imwrite('cartoon.jpg', cartoon)
+
+# Wait for key press and exit
 cv2.waitKey(0)
 cv2.destroyAllWindows()
