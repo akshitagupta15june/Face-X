51ea28e23d8bd374204ba9d6269c57bbe8637417
Abhishant Padam <abhishantpadam78@gmail.com>
2023-05-26 04:06:54 +0530

    added eye tracking on the eye

--- Eye Blink Counter/BlinkCounter.py
+++ Eye Blink Counter/BlinkCounter.py
@@ -6,14 +6,21 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 cap = cv2.VideoCapture('Video.mp4')
 detector = FaceMeshDetector(maxFaces = 1)
 
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+
 while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    img, faces = detector.findFaceMesh(img)
-
     success, img = cap.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    if faces:
+        face = faces[0]
+        for id in idList:
+            cv2.circle(img,face[id],3,(99,86,232),cv2.FILLED)
+
     img = cv2.resize(img, (640, 360))
     cv2.imshow("Image",img)
     cv2.waitKey(1)
