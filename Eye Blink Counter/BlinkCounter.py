9afb03d7b01b970dfc17050de905bb8bdd9e56b6
Abhishant Padam <abhishantpadam78@gmail.com>
2023-05-26 02:44:17 +0530

    in this commit i made the video into a loop so that when the final frame count it will be reset to frame 0 starting over again and again making it infinitely looped.

--- Eye Blink Counter/BlinkCounter.py
+++ Eye Blink Counter/BlinkCounter.py
@@ -4,6 +4,10 @@ import cvzone
 cap = cv2.VideoCapture('Video.mp4')
 
 while True:
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
     success, img = cap.read()
     img = cv2.resize(img, (640, 360))
     cv2.imshow("Image",img)
