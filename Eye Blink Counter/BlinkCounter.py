a049990eada22dc8a9e87b0a6af8b58dba544268
Abhishant Padam <abhishantpadam78@gmail.com>
2023-05-26 03:05:16 +0530

    added FaceMeshModule ot detect Faces(1) and modifies the img using findFaceMesh() to detect and see landmarks.

--- Eye Blink Counter/BlinkCounter.py
+++ Eye Blink Counter/BlinkCounter.py
@@ -1,12 +1,17 @@
 import cv2
 import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
+
 
 cap = cv2.VideoCapture('Video.mp4')
+detector = FaceMeshDetector(maxFaces = 1)
 
 while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    img, faces = detector.findFaceMesh(img)
 
     success, img = cap.read()
     img = cv2.resize(img, (640, 360))
