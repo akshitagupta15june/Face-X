6a18ff58ffd12ef88bce5bfbfbc7f3ef950c7509
Abhishant Padam <abhishantpadam78@gmail.com>
2023-05-27 01:34:50 +0530

    added horizontal and vertical distances to measure the blink.

--- Eye Blink Counter/BlinkCounter.py
+++ Eye Blink Counter/BlinkCounter.py
@@ -4,6 +4,8 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 
 
 cap = cv2.VideoCapture('Video.mp4')
+# For detecting the blink using webcam comment the above line and uncomment the below line.
+#cap = cv2.VideoCapture(2)  x
 detector = FaceMeshDetector(maxFaces = 1)
 
 idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
@@ -25,8 +27,13 @@ while True:
         leftDown = face[23]
         leftLeft = face[130]
         leftRight = face[243]
-        lengthHor,_ = detector.findDistance(leftUp,leftDown)
-        print(lengthHor)
+        lengthVer, _ = detector.findDistance(leftUp,leftDown)
+        lengthHor, _ = detector.findDistance(leftLeft,LeftRight)
+
+        cv2.line(img, leftUp, leftDown, (251,238,106), 1)
+        cv2.line(img, leftLeft, leftRight, (251,238,106), 1)
+ 
+        print(int((lengthVer/lengthHor)*100))
 
     img = cv2.resize(img, (640, 360))
     cv2.imshow("Image",img)
